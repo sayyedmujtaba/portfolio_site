29de30d5eee1420bda491ee28cc6ac16a4e28d8a
Syed Mujtaba <smujtabah14709@gmail.com>
2025-04-05 08:15:00 +0500

    Header section completed

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".nav-list ul");
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
 });
