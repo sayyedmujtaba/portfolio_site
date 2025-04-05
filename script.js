c1a3bf47b644d44b1d31b788c082e82521ece137
Syed Mujtaba <smujtabah14709@gmail.com>
2025-04-05 10:09:13 +0500

    53:14

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".nav-list ul");
+const menuItem = document.querySelectorAll(".nav-list ul li a");
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     mobileMenu.classList.toggle("active");
+});
+
+menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        mobileMenu.classList.remove("active");
+    });
 });
