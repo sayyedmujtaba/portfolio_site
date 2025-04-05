600947481c42f1304f55a663f6c3cf839c4966fa
Syed Mujtaba <smujtabah14709@gmail.com>
2025-04-05 21:01:40 +0500

    header and hero section done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".nav-list ul");
 const menuItem = document.querySelectorAll(".nav-list ul li a");
-
+const header = document.querySelector(".header");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -13,4 +13,13 @@ menuItem.forEach((item) => {
         hamburger.classList.remove("active");
         mobileMenu.classList.remove("active");
     });
+});
+
+document.addEventListener("scroll", () => {
+    var scrollPosition = window.scrollY;
+    if (scrollPosition > 250) {
+        header.style.backgroundColor = "#29323c";
+    } else {
+        header.style.backgroundColor = "transparent";
+    }
 });
